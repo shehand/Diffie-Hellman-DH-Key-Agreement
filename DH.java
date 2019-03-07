0c9f662ce70f578846f98846f3a75d9f51e168d5
shehand <sonodewcomplex@gmail.com>
2019-03-07 19:10:00 +0530

    add generatePublicKey class

--- DH.java
+++ DH.java
@@ -1,5 +1,15 @@
+import java.util.*;
+
 class DH{
 	
+	private int n = 7;
+	private int g = 31;
+	
+	public static int generatePublicKey(int privateKey){
+		
+		int publicKey = ( ( int ) Math.pow( g, privateKey ) ) % n;
+		return publicKey;
+	}
 	public static void main(String args[]){
 		
 	}
