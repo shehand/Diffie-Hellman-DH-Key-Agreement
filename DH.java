db95f24ab718e3c329a8a6147a60e5b74929cb43
shehand <sonodewcomplex@gmail.com>
2019-03-07 19:22:46 +0530

    fix bugs

--- DH.java
+++ DH.java
@@ -2,8 +2,8 @@ import java.util.*;
 
 class DH{
 	
-	private int n = 7;
-	private int g = 31;
+	private static int n = 23;
+	private static int g = 7;
 	
 	public static int generatePublicKey(int privateKey){
 		
@@ -20,22 +20,31 @@ class DH{
 	}
 	
 	public static void main(String args[]){
+		
 		if(args.length == 0){
+			
 			System.out.println("Missing arguments...");
 			System.out.println("Usage:  java DH [private_key]");
             System.out.println("Usage:  java DH [private_key] [public_key]");
+			
 		}else if(args.length == 1){
+			
 			DH dif = new DH();
-			int publicKey = dif.generatePublicKey(args[0]);
+			int publicKey = dif.generatePublicKey(Integer.parseInt(args[0]));
 			System.out.println("Public Key of the Session : " + publicKey );
+			
 		}else if(args.length == 2){
+			
 			DH dif = new DH();
-			int sessionKey = dif.generateSessionKey(args[0], args[1]);
+			int sessionKey = dif.generateSessionKey(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
 			System.out.println("Session Key of the Session : " + sessionKey );
+			
 		}else{
+			
 			System.out.println("Invalid arguments...");
 			System.out.println("Usage:  java DH [private_key]");
             System.out.println("Usage:  java DH [private_key] [public_key]");
+			
 		} 
 	}
 }
