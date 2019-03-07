3530fbaf52913fc367eefc0c0f54b555b13214e7
shehand <sonodewcomplex@gmail.com>
2019-03-07 19:16:36 +0530

    add operating algorithm

--- DH.java
+++ DH.java
@@ -20,6 +20,22 @@ class DH{
 	}
 	
 	public static void main(String args[]){
-		
+		if(args.length == 0){
+			System.out.println("Missing arguments...");
+			System.out.println("Usage:  java DH [private_key]");
+            System.out.println("Usage:  java DH [private_key] [public_key]");
+		}else if(args.length == 1){
+			DH dif = new DH();
+			int publicKey = dif.generatePublicKey(args[0]);
+			System.out.println("Public Key of the Session : " + publicKey );
+		}else if(args.length == 2){
+			DH dif = new DH();
+			int sessionKey = dif.generateSessionKey(args[0], args[1]);
+			System.out.println("Session Key of the Session : " + sessionKey );
+		}else{
+			System.out.println("Invalid arguments...");
+			System.out.println("Usage:  java DH [private_key]");
+            System.out.println("Usage:  java DH [private_key] [public_key]");
+		} 
 	}
 }
